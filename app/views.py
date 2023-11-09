c0e2350d134dd545df5a9d467c81e8e90e5dcc06
Ramesh9100 <chitrarameshgowd@gmail.com>
2023-11-09 19:19:38 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def bittu(request):
+    return render(request,'bittu.html')
+
+def chotu(request):
+    return render(request,'chotu.html')
+
+def ram(request):
+    return render(request,'ram.html')
+
+def seena(request):
+    return render(request,'seena.html')
